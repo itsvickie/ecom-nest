ad18bde8cbb29e6066c2755d650b5ff5812b77c4
Vickie <vcamilaxs@gmail.com>
2023-06-11 21:23:59 -0300

    fix: fixed dependencies nestjs

--- src/main.module.ts
+++ src/main.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
-import { JwtModule, JwtService } from '@nestjs/jwt';
+import { JwtService } from '@nestjs/jwt';
 
-import { PrismaModule } from '@Prisma/prisma.module';
 import { UserService } from './user/user.service';
 import { UserRepository } from './user/user.repository';
 import { PasswordHelper } from '@Helpers/password.hash';
@@ -9,17 +8,10 @@ import { UserController } from './user/user.controller';
 import { ProductService } from './product/product.service';
 import { ProductController } from './product/product.controller';
 import { ProductRepository } from './product/product.repository';
+import { EnvironmentService } from '@Envs/environments.service';
 
 @Module({
-  imports: [
-    PrismaModule,
-    JwtModule.register({
-      global: true,
-      // TODO: Botar a env
-      secret: 'secret',
-      signOptions: { expiresIn: '60s' },
-    }),
-  ],
+  imports: [],
   controllers: [ProductController, UserController],
   providers: [
     JwtService,
@@ -28,6 +20,7 @@ import { ProductRepository } from './product/product.repository';
     UserRepository,
     PasswordHelper,
     ProductRepository,
+    EnvironmentService,
   ],
 })
 export class MainModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '@Prisma/prisma.module';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { EnvironmentService } from '@Envs/environments.service';
 
 @Module({
   controllers: [UserController],
   providers: [UserService],
-  imports: [PrismaModule],
+  imports: [PrismaModule, EnvironmentService],
 })
 export class UserModule {}
